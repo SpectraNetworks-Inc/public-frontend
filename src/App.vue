15f4e41fdf8174aa35423ae36f3dceabd0dd2438
Tyree Zacharopoulos <tyree@tyree.ca>
2022-06-16 03:47:25 +0000

    DEV v1.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <HelloWorld msg="Hello"/>
+  <HelloWorld msg="Test"/>
 </template>
+
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -14,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
