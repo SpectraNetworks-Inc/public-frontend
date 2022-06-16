beb5f33ede9cbf467e4690635a1b7177627238e0
Tyree Zacharopoulos <tyree@tyree.ca>
2022-06-16 07:07:57 +0000

    test button

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -5,8 +5,9 @@
           <div class="navbar navbar-light">
             <div class="container-xl">
               <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="/home">
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/">
                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="5 12 3 12 12 3 21 12 19 12"></polyline><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path></svg>
                     </span>
@@ -15,6 +16,17 @@
                     </span>
                   </a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/test">
+                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="5 12 3 12 12 3 21 12 19 12"></polyline><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path></svg>
+                    </span>
+                    <span class="nav-link-title">
+                      Test
+                    </span>
+                  </a>
+                </li>
+
               </ul>
 
               <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
