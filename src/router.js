2bbb9390c42fc0bab9935bf3927d0318b52c538c
Tyree Zacharopoulos <tyree@tyree.ca>
2022-06-16 23:30:17 +0000

    catchAll page (404)

--- src/router.js
+++ src/router.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
+import RootPage from './pages/HomePage.vue';
 import HomePage from './pages/HomePage.vue';
 import TestPage from './pages/TestPage.vue';
+
+import NotFound from './pages/errors/404Page.vue'
 
 
 const routes = [
   {
     path: "/",
+    name: "RootPage",
+    component: RootPage
+  },
+  {
+    path: "/home",
     name: "HomePage",
     component: HomePage
   },
@@ -13,7 +21,12 @@ const routes = [
     path: "/test",
     name: "TestPage",
     component: TestPage
-  }
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+}
 ];
 
 const router = createRouter({
