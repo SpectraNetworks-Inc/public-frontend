a866cc62d3d6d7c35b104020d3b85a0c7908025c
Tyree Zacharopoulos <tyree@tyree.ca>
2022-06-16 02:21:20 +0000

    test

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,14 +1,25 @@
-export async function onRequest(context) {
-    // Contents of context object
-    const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
-    } = context;
-  
-    return new Response("Hello, world!");
+const cookieName = "ab-test-cookie"
+const newHomepagePathName = "/test"
+
+const abTest = async ({request, next, env}) => {
+  /*
+  Todo: 
+  1. Assign cookies based on randomly genrated percentage, then serve
+  */
+
+  const url = new URL(request.url)
+  if (url.pathname === "/") {
+    // if cookie ab-test-cookie=new then change the request to go to /test
+    // if no cookie set, pass x% of traffic and set a cookie value to "current" or "new"
+
+    let cookie = request.headers.get("cookie")
+    // is cookie set?
+    if (cookie && cookie.includes(`${cookieName}=new`)) {
+      // Change the request to go to /test (as set in the newHomepagePathName variable)
+      url.pathname = newHomepagePathName
+      return env.ASSETS.fetch(url)
+    }
   }
-  
+}
+
+export const onRequest = [abTest]
